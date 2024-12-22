d56d6380c2f3fedf4b93ebab09a47d89be24d941
Juan Esteban Guevara <Juan.Guevara@WLAPCOING94.local>
2024-12-22 17:40:30 -0500

    fix(main): Added Main.c commit with the updated use of the fms.h header file

--- Src/main.c
+++ Src/main.c
@@ -3,61 +3,9 @@
 #include "gpio.h"
 #include "systick.h"
 #include "uart.h"
+#include "fms.h"
 
-#define TEMP_UNLOCK_DURATION 5000 // Duration in ms for temporary unlock
 
-typedef enum {
-    LOCKED,
-    TEMP_UNLOCK,
-    PERM_UNLOCK
-} DoorState_t;
-
-DoorState_t current_state = LOCKED;
-uint32_t unlock_timer = 0;
-
-void run_state_machine(void) {
-    switch (current_state) {
-        case LOCKED:
-            // No periodic action in locked state
-            break;
-        case TEMP_UNLOCK:
-            if (systick_GetTick() - unlock_timer >= TEMP_UNLOCK_DURATION) {
-                gpio_set_door_led_state(0); // Turn off door state LED
-                current_state = LOCKED;
-            }
-            break;
-        case PERM_UNLOCK:
-            // No periodic action in permanent unlock state
-            break;
-    }
-}
-
-void handle_event(uint8_t event) {
-    if (event == 1) { // Single button press
-        if(gpio_get_door_led_state()){
-            current_state = LOCKED;
-            gpio_set_door_led_state(0);
-            return;
-        }
-        gpio_set_door_led_state(1); // Turn on door state LED
-        current_state = TEMP_UNLOCK;
-        usart2_send_string("Single touch!\r\n");
-        unlock_timer = systick_GetTick();
-    } else if (event == 2) { // Double button press
-        gpio_set_door_led_state(1); // Turn on door state LED
-        current_state = PERM_UNLOCK;
-        usart2_send_string("Double touch!\r\n");
-    } else if (event == 'O') { // UART OPEN command
-        gpio_set_door_led_state(1); // Turn on door state LED
-        current_state = TEMP_UNLOCK;
-        usart2_send_string("UART OPEN!\r\n");
-        unlock_timer = systick_GetTick();
-    } else if (event == 'C') { // UART CLOSE command
-        gpio_set_door_led_state(0); // Turn off door state LED
-        current_state = LOCKED;
-        usart2_send_string("UART CLOSE!\r\n");
-    }
-}
 
 int main(void) {
     configure_systick_and_start();
